6f36e618fdcaba6eb94b94aaf6f279e84b404fcf
sssssssiiiiinnn <ygddjklollb@gmail.com>
2020-09-09 21:40:59 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input
+        type="text"
+        onClick={() => {
+          console.log("asdfa");
+        }}
+        onChange={(event) => {
+          console.log(event.target);
+        }}
+      />
+    </React.Fragment>
   );
 }
 
