525af8c2aa11acf374753ff555be6fef98934de5
sssssssiiiiinnn <ygddjklollb@gmail.com>
2020-09-09 22:24:51 +0900

    create class components and functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onClick={() => {
+//           console.log("asdfa");
+//         }}
+//         onChange={(event) => {
+//           console.log(event.target);
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onClick={() => {
-          console.log("asdfa");
-        }}
-        onChange={(event) => {
-          console.log(event.target);
-        }}
-      />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+    </div>
   );
-}
+};
+
+const Cat = () => {
+  return (
+    <div>
+      <h1>Meow!</h1>
+      <h1>Meow!</h1>
+      <h1>Meow!</h1>
+    </div>
+  );
+};
 
 export default App;
